f1ef5c2e15f2035b6e89ac1f1f46ef682141b6ad
johnmetzbower <johnmetzbower@nokware.net>
2015-01-17 13:56:38 -0600

    Some int Create string

---
+++ Array Practice/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] hiddenWords = { 8, 0, 0, 8, 5 };
+            for (int index = 0; index < hiddenWords.Length; index++)
+            {
+                int word = hiddenWords[index];
+                Console.WriteLine(word);
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
